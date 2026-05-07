3772559c362f96051f76b2ce1d805ab2cda3db97
Pawel Wlodarczyk <150594946+Wlodar91@users.noreply.github.com>
2026-05-07 10:20:16 +0200

    Update target URL in REDIRECT_CONFIG

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // To jest jedyne miejsce, które zwykle trzeba zmienić.
 
 window.REDIRECT_CONFIG = {
-  targetUrl: "https://www.otodom.pl/pl/oferta/TUTAJ-WKLEJ-LINK-DO-OFERTY",
+  targetUrl: "https://www.otodom.pl/pl/oferta/apartament-83-m-zielony-taras-na-dachu-80-m-bezposrednio-ID4AOkB",
 
   // Zalecane: false
   // false = użytkownik może użyć przycisku „wstecz” i wrócić z Otodom
